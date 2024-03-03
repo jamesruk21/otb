56152293bb710a0ec27862a63c84e12f0071f93c
jamesruk21@gmail.com <jamesruk21@gmail.com>
2024-03-03 08:14:27 +0000

    Created project, added main class, added test class with 3 customer tests as per challenge doc

---
+++ HolidaySearch/HolidaySearch.Tests/HolidaySearchTests.cs
@@ -0,0 +1,47 @@
+namespace Holiday.Tests
+{
+    [Fact]
+    public void Results_Returns_BestValueHoliday_Customer1()
+    {
+        // Arrange
+        var holidaySearch = new HolidaySearch();
+        var departingAirport = new List<string> { "MAN" };
+
+        // Act
+        var results = holidaySearch.Results(departingAirport, "AGP", new DateTime(2023, 7, 1), 7);
+
+        // Assert
+        Assert.Equal(2, results.First().Flight.Id);
+        Assert.Equal(9, results.First().Hotel.Id);
+    }
+
+    [Fact]
+    public void Results_Returns_BestValueHoliday_Customer2()
+    {
+        // Arrange
+        var holidaySearch = new HolidaySearch();
+        var departingAirport = new List<string> { "LTN", "LGW" };
+
+        // Act
+        var results = holidaySearch.Results(departingAirport, "PMI", new DateTime(2023, 6, 15), 10);
+
+        // Assert
+        Assert.Equal(6, results.First().Flight.Id);
+        Assert.Equal(5, results.First().Hotel.Id);
+    }
+
+    [Fact]
+    public void Results_Returns_BestValueHoliday_Customer3()
+    {
+        // Arrange
+        var holidaySearch = new HolidaySearch();
+        var departingAirport = new List<string>();
+
+        // Act
+        var results = holidaySearch.Results(departingAirport, "LPA", new DateTime(2022, 11, 10), 14);
+
+        // Assert
+        Assert.Equal(7, results.First().Flight.Id);
+        Assert.Equal(6, results.First().Hotel.Id);
+    }
+}
---
+++ HolidaySearch/HolidaySearch/HolidaySearch.cs
@@ -0,0 +1,7 @@
+﻿namespace Holiday
+{
+    public class HolidaySearch
+    {
+
+    }
+}
